6cb083f72aa990127b5d8b5204ad1959d16d3d42
Jojo <jojothomas1515@gmail.com>
2023-06-03 11:44:48 +0100

    added a little dynamic touch

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+  let amenities = [];
+  // sets the name of all previously ticked amenities
+  $(".amenities input[type='checkbox']").each(function () {
+    const ids = $(this).attr('data-id');
+    if (this.checked) {
+      amenities.push(ids);
+    } else {
+      if (amenities.includes(ids)) {
+        amenities = amenities.filter(val => val !== ids);
+      }
+    }
+    $('.amenities h4').text(amenities.map(item => {
+      return $(`[data-id=${item}]`).attr('data-name');
+    }).join(', '));
+  });
+  // Add or remove  amenities from h4 depending on if it's checked or not
+  $(".amenities input[type='checkbox']").bind('change', function (e) {
+    const ids = $(e.target).attr('data-id');
+    if (e.target.checked) {
+      amenities.push(ids);
+    } else {
+      if (amenities.includes(ids)) {
+        amenities = amenities.filter(val => val !== ids);
+      }
+    }
+    $('.amenities h4').text(amenities.map(item => {
+      return $(`[data-id=${item}]`).attr('data-name');
+    }).join(', '));
+  });
+});
